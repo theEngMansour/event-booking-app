ddf4487cc74e658d47003aedac126b554a8d7d8e
Mansour Ahmed <73757452+theEngMansour@users.noreply.github.com>
2022-10-10 23:14:58 +0300

    sort coded

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,4 +1,4 @@
-import prisma from "lib/prisma"; 
+import prisma from "lib/prisma";
 import micro_cors from "micro-cors";
 import jwt from "jsonwebtoken";
 import { ApolloServer } from "apollo-server-micro";
@@ -6,37 +6,36 @@ import { typeDefs } from "schema";
 import { resolvers } from "resolvers";
 
 const cors = micro_cors({
-  origin:"https://studio.apollographql.com", 
-  allowMethods:["GET","POST"]
-})
+  origin: "https://studio.apollographql.com",
+  allowMethods: ["GET", "POST"],
+});
 
 const apolloServer = new ApolloServer({
-  typeDefs, 
-  resolvers, 
+  typeDefs,
+  resolvers,
   context: async ({ req }) => {
-    const auth = req ? req.headers.authorization : null
-    
-    if(auth) {
-      const decoded = jwt.verify(auth, process.env.JWT)
-      const user = await prisma.user.findUnique({ where: { id: decoded.id }})
-      return { user }
+    const auth = req ? req.headers.authorization : null;
+    if (auth) {
+      const decoded = jwt.verify(auth, process.env.JWT);
+      const user = await prisma.user.findUnique({ where: { id: decoded.id } });
+      return { user };
     }
-  }
-})
+  },
+});
 
-const startServer = apolloServer.start()
+const startServer = apolloServer.start();
 
 export default cors(async function handler(req, res) {
-  if(req.method == "OPTIONS") {
-    res.end()
-    return false
+  if (req.method == "OPTIONS") {
+    res.end();
+    return false;
   }
   await startServer;
-  await apolloServer.createHandler({ path: "/api/graphql" })(req, res)
-})
+  await apolloServer.createHandler({ path: "/api/graphql" })(req, res);
+});
 
 export const config = {
   api: {
     bodyParser: false,
   },
-}
+};
